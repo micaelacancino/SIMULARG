9b38bb23cc82522b12d3781b371ef83aa5ba39d4
micaela <cancinomicaelaanahi@gmail.com>
2026-05-27 21:54:39 -0700

    recoleccion zona 1 y 2

--- simulador/simulador.js
+++ simulador/simulador.js
@@ -31,8 +31,43 @@ let CCPUR = 0;
 let GPU = 0;
 let CGPURA = 0;
 let CGPUR = 0;
+ let  KMZ1, KMZ2; //km recorridos 
+let DVZ1, DVZ2;  //duración del viaje
 
 const u = lehmer(); // genera un número entre 0 y 1 usando el método Lehmer
 
-while(u<= 0.19)
+if(u<= 0.19){
+   //ruta adversa
+  
+    const u2 = lehmer(); // genera otro número entre 0 y 1 para determinar la ruta adversa específica
+      KMZ1 = 25.2 + 3.4 * u2;
 
+     const u3 = lehmer();
+     KMZ2 = 45.8 + 7.5 * u3;
+ 
+    DVZ1 = (KMZ1 * 50) / 25.5
+    DVZ2 = (KMZ2 * 2.77) / 45.8
+   //S= "Adversa"
+       
+}else {
+  // ruta normal
+  KMZ1 = 25.2;
+  KMZ2 = 45.8;
+  DVZ1 = 50;
+  DVZ2= 77;
+  //S= "Normal"
+}
+
+//combustible (variable)
+const u4 = lehmer();
+const LCZ1 = 24 + 11 * u4;
+
+const u5 = lehmer();
+const LCZ2 = 24 + 11 * u5;
+
+//precio combustible
+const PCZ1 = ((KMZ1 * LCZ1) / 100) * 2248;
+const PCZ2 = ((KMZ2 * LCZ2) / 100) * 2248;
+
+    }
+ }
